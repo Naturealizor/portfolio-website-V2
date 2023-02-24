148a0d25b3bdde5139b73ea4341b70464a886852
Jacob Jordan <31334963+Naturealizor@users.noreply.github.com>
2023-02-23 19:14:54 -0500

    trying to concatenate questions and inputs

--- src/routes/rofosurvey/+page.svelte
+++ src/routes/rofosurvey/+page.svelte
@@ -8,10 +8,30 @@
 	function submitForm() {
 		// save the inputs to a txt file
 		const link = document.createElement('a');
+		const questionInputs = document.querySelectorAll('#question');
 		const inputs = document.querySelectorAll('#form-input');
-		const content = Array.from(inputs)
-			.map((input) => input.value)
-			.join('\n');
+
+		// let content = '';
+		// for (let i = 0; i < questionInputs.length; i++) {
+		// 	content += questionInputs[i].textContent + ': ' + inputs[i].value + '\n';
+		// }
+		//create new const to itterate through each question as an array
+		const questions = [];
+		questionInputs.forEach((question) => {
+			questions.push(question.innerText);
+		});
+		const content =
+			questions.join('\n') +
+			'\n' +
+			Array.from(inputs)
+				.map((input) => input.value)
+				.join('\n');
+		// const content =
+		// 	questions.join('\n') +
+		// 	'\n' +
+		// 	Array.from(inputs)
+		// 		.map((input) => input.value)
+		// 		.join('\n');
 		const file = new Blob([content], { type: 'text/plain' });
 		link.href = URL.createObjectURL(file);
 		link.download = 'rofoSurvey.txt';
@@ -25,20 +45,22 @@
 <h2 class="uppercase m-5 bg-yellow-200">e-stop info</h2>
 <br />
 <div class="flex">
-	<h3 class="ml-6 text-lg mr-5">Store #</h3>
+	<h3 class="ml-6 text-lg mr-5" id="question">Store #</h3>
 	<input class="border-4 " type="text" id="form-input" name="storeNum" />
 </div>
 <div class="m-6 p-2 grid">
-	<label for="totalEstops">How many E-Stops are at this store</label>
+	<label for="totalEstops" id="question">How many E-Stops are at this store</label>
 	<input class="border-4 m-2 pr-1" type="num" id="form-input" name="totalEstops" />
 	<br />
-	<label for="outsideBtns">Outside:</label>
+	<label for="outsideBtns" id="question">Outside:</label>
 	<input class="border-4 m-2 pr-1" type="num" id="form-input" name="outsideBtns" />
 	<br />
-	<label for="insideBtns">Inside:</label>
+	<label for="insideBtns" id="question">Inside:</label>
 	<input class="border-4 m-2 pr-4" type="num" id="form-input" name="insideBtns" />
 	<br />
-	<label class="m-2" for="totalEstops">Do the E-Stop buttons contain a reset button? </label>
+	<label class="m-2" for="totalEstops" id="question"
+		>Do the E-Stop buttons contain a reset button?
+	</label>
 	<input
 		type="textarea"
 		for="resetEstop"
@@ -48,20 +70,23 @@
 		class="border-4 ml-2"
 	/>
 	<br />
-	<label class="m-2" for="eResetLoc">If yes what is the location?</label>
+	<label class="m-2" for="eResetLoc" id="question">If yes what is the location?</label>
 	<input class="border-4 m-2 pr-4" type="text" id="form-input" name="eResetLoc" />
 	<br />
 	<br />
 </div>
 <h2 class="uppercase m-5 bg-yellow-200">Dispenser Manager</h2>
 <div class="m-6 p-2 grid">
-	<h3 id="form-input">Does the site have a Power Integrity, SSDI or an outside control box?</h3>
+	<h3 id="question">Does the site have a Power Integrity, SSDI or an outside control box?</h3>
 	<input type="textarea" id="form-input" name="pwrType" class="border-4" />
 	<br />
-	<label for="totalEstops">Is there a “fuel reset” button on the face of the unit? </label>
+	<label for="totalEstops" id="question"
+		>Is there a “fuel reset” button on the face of the unit?
+	</label>
 	<input class="border-4 m-2 pr-1" type="num" id="form-input" name="outsideBtns" />
 	<br />
-	<label for="totalEstops">If unit is outside where is the main reset for the E-Stop circuit?</label
+	<label for="totalEstops" id="question"
+		>If unit is outside where is the main reset for the E-Stop circuit?</label
 	>
 	<input class="border-4 m-2 pr-4" type="num" id="form-input" name="insideBtns" />
 	<br />
@@ -69,60 +94,66 @@
 </div>
 <h2 class="uppercase m-5 bg-yellow-200">motor control and dispenser info</h2>
 <div class="m-6 p-2 grid">
-	<label for="motorStyle"
+	<label for="motorStyle" id="question"
 		>What style motor controllers are at the site? (i.e. MAGVFC, STP-IST, STP-CBS, Red Jacket etc..)</label
 	>
 	<input class="border-4 m-2 pr-4" type="text" id="form-input" name="motorStyle" />
 	<br />
-	<label for="otherStyle">If other please specify type</label>
+	<label for="otherStyle" id="question">If other please specify type</label>
 	<input class="border-4 m-2 pr-4" type="text" id="form-input" name="otherStyle" />
 	<br />
-	<label for="motorLoc"
+	<label for="motorLoc" id="question"
 		>Where are the motors controllers located and are they easily accessible?
 	</label>
 	<input class="border-4 m-2 pr-4" type="text" id="form-input" name="motorLoc" />
 	<br />
-	<label for="motorAccess">ACCESSIBLE?</label>
+	<label for="motorAccess" id="question">ACCESSIBLE?</label>
 	<input class="border-4 m-2 pr-4" type="text" id="form-input" name="motorAccess" />
 	<br />
-	<label for="stpBreakers">Are the breakers labelled correctly for the STPs?</label>
+	<label for="stpBreakers" id="question">Are the breakers labelled correctly for the STPs?</label>
 	<input class="border-4 m-2 pr-4" type="text" id="form-input" name="stpBreakers" />
 	<br />
-	<label for="stpBreakers">List panel location, panel letter, and breaker#</label>
+	<label for="stpBreakers" id="question">List panel location, panel letter, and breaker#</label>
 	<input class="border-4 m-2 pr-4" type="text" id="form-input" name="stpBreakers" />
 	<br />
-	<label for="fuelBreakers">Check labels for fuel dispensers and list panel info</label>
+	<label for="fuelBreakers" id="question"
+		>Check labels for fuel dispensers and list panel info</label
+	>
 	<input class="border-4 m-2 pr-4" type="text" id="form-input" name="fuelBreakers" />
 	<br />
 </div>
 <h2 class="uppercase m-5 bg-yellow-200">tank monitor info</h2>
 <div class="m-6 p-2 grid">
-	<label for="tankModel">What model tank monitor is at the site</label>
+	<label for="tankModel" id="question">What model tank monitor is at the site</label>
 	<input class="border-4 m-2 pr-4" type="text" id="form-input" name="tankModel" />
 	<br />
-	<label for="tankLoc">Where is the tank monitor located?</label>
+	<label for="tankLoc" id="question">Where is the tank monitor located?</label>
 	<input class="border-4 m-2 pr-4" type="text" id="form-input" name="tankLoc" />
 	<br />
-	<label for="tankAccess">Where is the tank monitor breaker located and how is it labelled?</label>
+	<label for="tankAccess" id="question"
+		>Where is the tank monitor breaker located and how is it labelled?</label
+	>
 	<input class="border-4 m-2 pr-4" type="text" id="form-input" name="tankAccess" />
 	<br />
 </div>
 <h2 class="uppercase m-5 bg-yellow-200">panther box and communication info</h2>
 <div class="m-6 p-2 grid">
-	<label for="pantherGrounded"
+	<label for="pantherGrounded" id="question"
 		>Is the Panther box plugged into a Mac Victor isolated ground receptacle (Orange plug)?</label
 	>
 	<input class="border-4 m-2 pr-4" type="text" id="form-input" name="pantherGrounded" />
 	<br />
-	<label for="pantherVoltage">Is there a low voltage disconnect box installed at the site?</label>
+	<label for="pantherVoltage" id="question"
+		>Is there a low voltage disconnect box installed at the site?</label
+	>
 	<input class="border-4 m-2 pr-4" type="text" id="form-input" name="pantherVoltage" />
 	<br />
-	<label for="pantherVoltage"
+	<label for="pantherVoltage" id="question"
 		>Does the site have Wayne Connect, Switch, or Wireless installed for ICR coms?</label
 	>
 	<input class="border-4 m-2 pr-4" type="text" id="form-input" name="pantherVoltage" />
 	<br />
-	<label for="pantherLoc">Where is the above equipment located?</label>
+	<label for="pantherLoc" id="question">Where is the above equipment located?</label>
 	<input class="border-4 m-2 pr-4" type="text" id="form-input" name="pantherLoc" />
 	<br />
 </div>
